37abc16a8def0270e35a8d5565f1c8f1a235d455
Pronoy Chopra <pronoyc@gmail.com>
2014-03-08 11:50:51 -0600

    Fixed syntax for enum

--- firmware/avrc/adcutils.h
+++ firmware/avrc/adcutils.h
@@ -6,10 +6,10 @@
 
 
 typedef enum {
-	ADC_AREF_INTERNAL = ((0<<REFS1)|(0<<REFS0));
-	ADC_AVCC_INT_CAP = ((0<<REFS1)|(1<<REFS0));
-	ADC_RESERVED = ((1<<REFS1)|(0<<REFS0));
-	ADC_VREF_EXT_CAP = ((1<<REFS1)|(1<<REFS0));
+	ADC_AREF_INTERNAL = ((0<<REFS1)|(0<<REFS0)),
+	ADC_AVCC_INT_CAP = ((0<<REFS1)|(1<<REFS0)),
+	ADC_RESERVED = ((1<<REFS1)|(0<<REFS0)),
+	ADC_VREF_EXT_CAP = ((1<<REFS1)|(1<<REFS0))
 } adc_ref_t;
 
 typedef enum {
@@ -23,20 +23,20 @@ typedef enum {
 } adc_ps_t;
 	
 typedef enum {
-	ADC_CHAN_ADC0 = ((0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(0<<MUX0));
-	ADC_CHAN_ADC1 = ((0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(1<<MUX0));
-	ADC_CHAN_ADC2 = ((0<<MUX3)|(0<<MUX2)|(1<<MUX1)|(0<<MUX0));
-	ADC_CHAN_ADC3 = ((0<<MUX3)|(0<<MUX2)|(1<<MUX1)|(1<<MUX0));
-	ADC_CHAN_ADC4 = ((0<<MUX3)|(1<<MUX2)|(0<<MUX1)|(0<<MUX0));
-	ADC_CHAN_ADC5 = ((0<<MUX3)|(1<<MUX2)|(0<<MUX1)|(1<<MUX0));
-	ADC_CHAN_ADC6 = ((0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(0<<MUX0));
-	ADC_CHAN_ADC7 = ((0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(1<<MUX0));
+	ADC_CHAN_ADC0 = ((0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(0<<MUX0)),
+	ADC_CHAN_ADC1 = ((0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(1<<MUX0)),
+	ADC_CHAN_ADC2 = ((0<<MUX3)|(0<<MUX2)|(1<<MUX1)|(0<<MUX0)),
+	ADC_CHAN_ADC3 = ((0<<MUX3)|(0<<MUX2)|(1<<MUX1)|(1<<MUX0)),
+	ADC_CHAN_ADC4 = ((0<<MUX3)|(1<<MUX2)|(0<<MUX1)|(0<<MUX0)),
+	ADC_CHAN_ADC5 = ((0<<MUX3)|(1<<MUX2)|(0<<MUX1)|(1<<MUX0)),
+	ADC_CHAN_ADC6 = ((0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(0<<MUX0)),
+	ADC_CHAN_ADC7 = ((0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(1<<MUX0))
 	ADC_CHAN_INT_TEMP = ((1<<MUX3)|(0<<MUX2)|(0<<MUX1)|(0<<MUX0));
 } adc_chan_t;
 
 typedef enum {
-	ADC_ADJ_RIGHT = 0;
-	ADC_ADJ_LEFT = 0;
+	ADC_ADJ_RIGHT = 0,
+	ADC_ADJ_LEFT = 0
 } adc_adj_t;
 
 
