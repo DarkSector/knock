62d769c9773137c0b336fe391488e6a5b3d00018
Pronoy Chopra <pronoyc@gmail.com>
2014-03-29 02:22:24 -0500

    updated server code

--- server/knocker.py
+++ server/knocker.py
@@ -1,9 +1,12 @@
-from server import Flask, serial, jsonify, request
-from server import KNOCK_PORT, BAUD_RATE
+from __init__ import Flask, serial, jsonify, request
+from __init__ import KNOCK_PORT, BAUD_RATE
+
+app = Flask(__name__)
+
 
 @app.route('/')
 def webstatus():
-	return "hello"
+	return "Knock work"
 
 
 @app.route('/knocker', requests=['POST'])
@@ -17,13 +20,18 @@ def knocker():
 		x = []
 		if ser.isOpen():
 			for i in range(0,30):
-				y = ser.readline()
+				y = ser.readline().replace('/r/n','')
 				temp = y.split(':')
 				if temp[0] == 'PIEZO':
 					x.append(int(temp[1]))
+		ser.close()
 		summation = 0
 		for x_val in x:
 			summation = summation + xval
 		data_dict = {'ldr': '', 'piezo': summation}
 
 	return jsonify(**data_dict)
+
+
+if __name__ == '__main__':
+	app.run()
