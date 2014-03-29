54656503fd156d2041262d0aa7b20f1e12455701
Pronoy Chopra <pronoyc@gmail.com>
2014-03-29 02:45:51 -0500

    test methods

--- server/knocker.py
+++ server/knocker.py
@@ -16,24 +16,24 @@ def webstatus():
 @app.route('/knocker', methods=['POST'])
 def knocker():
 	if request.method == 'POST':
-		ser = serial.Serial()
-		ser.port = KNOCK_PORT
-		ser.baudrate = BAUD_RATE
-		ser.open()
-		threshold = 10
-		x = []
-		if ser.isOpen():
-			for i in range(0,30):
-				y = ser.readline().replace('/r/n','')
-				temp = y.split(':')
-				if temp[0] == 'PIEZO':
-					x.append(int(temp[1]))
-		ser.close()
-		summation = 0
-		for x_val in x:
-			summation = summation + xval
-		data_dict = {'ldr': '', 'piezo': summation}
-
+		# ser = serial.Serial()
+		# ser.port = KNOCK_PORT
+		# ser.baudrate = BAUD_RATE
+		# ser.open()
+		# threshold = 10
+		# x = []
+		# if ser.isOpen():
+		# 	for i in range(0,30):
+		# 		y = ser.readline().replace('/r/n','')
+		# 		temp = y.split(':')
+		# 		if temp[0] == 'PIEZO':
+		# 			x.append(int(temp[1]))
+		# ser.close()
+		# summation = 0
+		# for x_val in x:
+		# 	summation = summation + xval
+		# data_dict = {'ldr': '', 'piezo': summation}
+		data_dict = {'ldr': '', 'piezo': 'foo'}
 	return jsonify(**data_dict)
 
 
