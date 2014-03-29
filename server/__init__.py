ce726e2b86c9bd9dbe11676e12331e3b6442174c
Pronoy Chopra <pronoyc@gmail.com>
2014-03-28 23:56:44 -0500

    updated code

--- server/__init__.py
+++ server/__init__.py
@@ -1,1 +1,5 @@
-KNOCK_PORT = ''
+import os
+import serial
+from flask import Flask, jsonify, request, render_template
+KNOCK_PORT = '/dev/tty.usbserial-A900XXQH'
+BAUD_RATE = 19200
--- server/knocker.py
+++ server/knocker.py
@@ -1,5 +1,29 @@
-import os
-import serial
-from flask import flask
+from server import Flask, serial, jsonify, request
+from server import KNOCK_PORT, BAUD_RATE
 
 @app.route('/')
+def webstatus():
+	return "hello"
+
+
+@app.route('/knocker', requests=['POST'])
+def knocker():
+	if request.method == 'POST':		
+		ser = serial.Serial()
+		ser.port = KNOCK_PORT
+		ser.baudrate = BAUD_RATE
+		ser.open()
+		threshold = 10
+		x = []
+		if ser.isOpen():
+			for i in range(0,30):
+				y = ser.readline()
+				temp = y.split(':')
+				if temp[0] == 'PIEZO':
+					x.append(int(temp[1]))
+		summation = 0
+		for x_val in x:
+			summation = summation + xval
+		data_dict = {'ldr': '', 'piezo': summation}
+
+	return jsonify(**data_dict)
