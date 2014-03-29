a30cfcd988e054b4f76e9edc993451c676313f02
Pronoy Chopra <pronoyc@gmail.com>
2014-03-28 22:47:45 -0500

    updated according to working code in atmel solution

--- firmware/avrc/main.c
+++ firmware/avrc/main.c
@@ -1,32 +1,65 @@
+/*
+ * main.c
+ *
+ * Created: 3/19/2014 10:51:09 PM
+ *  Author: DarkSector
+ */ 
+
 #define F_CPU 8000000UL
+#define BAUD 9600
 
 #include <avr/io.h>
+#include "adcutils.h"
 #include "customutils.h"
 #include "serialutils.h"
-#include "adcutils.h"
 #include <stdio.h>
 
-char outs[20];
+#define PIEZO ADC_CHAN_ADC5
+#define LDR_SENSOR ADC_CHAN_ADC4
 
-int main(void){
-	init_led();	
-	
-	USART_Init(UBBR_9600);
-	USART_tx_string("Connected!\r\n");    // we're alive!
-	
-	adc_chan_select(ADC_CHAN_INT_TEMP);
-	adc_init(ADC_VREF_EXT_CAP, ADC_PS_32, ADC_ADJ_LEFT);
+char ldr_outs[20];
+char piezo_outs[20];
+//volatile unsigned int adc_data
+unsigned int ldr_data;
+unsigned int piezo_data;
+
+int main(void)
+{
+	init_led();
+	adc_init(ADC_AREF_EXT_CAP, ADC_PS_32, ADC_ADJ_LEFT);
+	//adc_chan_select(LDR_SENSOR);
 	adc_enable();
-
-	blink_led(700, 3);
-
-	while(1){
-
-		unsigned int adc_data = 0;
-		adc_data = adc_read();
-		snprintf(outs,sizeof(outs),"%3d\r\n", adc_data);  // print it
-    	USART_tx_string(outs);
+	blink_led(400, 3);
+	USART_Init(UBBR);
+	USART_tx_string("Connected!\n");
+	//sei();
+    while(1)
+    {
 		
-	}
-	return 0;
+		ldr_data = 0;
+		piezo_data = 0;
+		adc_chan_select(LDR_SENSOR);
+		ldr_data = adc_read();
+		//delay_ms(25);
+		
+
+		
+		if(ldr_data <= 100){
+			// Implies ambient light is off
+			led_on();			
+		}
+		else{
+			// Implies ambient light is ON
+			led_off();
+		}
+		snprintf(ldr_outs,sizeof(ldr_outs),"LDR:%3d\r\n", ldr_data);  // print ldr data		
+		USART_tx_string(ldr_outs);
+			
+		adc_chan_select(PIEZO);
+		piezo_data = adc_read();
+		
+		snprintf(piezo_outs,sizeof(piezo_outs),"PIEZO:%3d\r\n", piezo_data);  // print piezo data		
+		USART_tx_string(piezo_outs);
+    }
 }
+
